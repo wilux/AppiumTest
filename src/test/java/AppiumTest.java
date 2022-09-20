2ca14e5d424808fef90df86c30c18cdeb623cd61
Nestor Flores <nestorchoele@gmail.com>
2022-09-20 10:53:25 -0300

    add sample

--- src/test/java/AppiumTest.java
+++ src/test/java/AppiumTest.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class AppiumTest {
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumTest {
+    private AndroidDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appium:deviceName", "sdk_gphone64_x86_64");
+        desiredCapabilities.setCapability("appium:udid", "emulator-5554");
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+    }
+
+    @Test
+    public void sampleTest() {
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
+
