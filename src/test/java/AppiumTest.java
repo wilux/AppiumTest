7ee6f40981bc21a61047de87078b1ee72f3027be
Nestor Flores <nestorchoele@gmail.com>
2022-09-20 16:40:03 -0300

    add sample

--- src/test/java/AppiumTest.java
+++ src/test/java/AppiumTest.java
@@ -1,37 +1,54 @@
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class AppiumTest {
-    private AndroidDriver driver;
+//    private AndroidDriver driver;
 
-    @Before
+    public AndroidDriver<MobileElement> driver;
+    public WebDriverWait wait;
+
+    @BeforeMethod
     public void setUp() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("appium:deviceName", "sdk_gphone64_x86_64");
+        desiredCapabilities.setCapability("appium:deviceName", "Android");
         desiredCapabilities.setCapability("appium:udid", "emulator-5554");
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
         desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
         desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
         desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+        desiredCapabilities.setCapability("appPackage", "com.poincenot.bpn.mobile.qa");
+        desiredCapabilities.setCapability("appActivity", "com.poincenot.doit.controller.activity.SplashActivity");
+        desiredCapabilities.setCapability("autoGrantPermissions", "true");
 
         URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+
     }
 
     @Test
     public void sampleTest() {
 
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginPage.userInput));
+
+        driver.findElement(LoginPage.userInput).sendKeys("test");
+        driver.findElement(LoginPage.passwordInput).sendKeys("test");
+        driver.findElement(LoginPage.submitButton).click();
     }
 
-    @After
+    @AfterTest
     public void tearDown() {
         driver.quit();
     }
---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.By;
+
+public class LoginPage {
+
+    public static final By userInput = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[1]");
+    public static final By passwordInput = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[2]");
+    public static final By submitButton = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[6]");
+}
