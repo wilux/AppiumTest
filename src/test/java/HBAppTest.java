85803f0051ba2b3fec8d7b44ca3cabcf2b7aac58
Nestor Flores <nestorchoele@gmail.com>
2022-09-21 08:24:24 -0300

    add sample

--- src/test/java/HBAppTest.java
+++ src/test/java/HBAppTest.java
@@ -3,16 +3,16 @@ import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-public class AppiumTest {
-//    private AndroidDriver driver;
-
+public class HBAppTest {
     public AndroidDriver<MobileElement> driver;
     public WebDriverWait wait;
 
@@ -21,6 +21,7 @@ public class AppiumTest {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("appium:deviceName", "Android");
         desiredCapabilities.setCapability("appium:udid", "emulator-5554");
+//        desiredCapabilities.setCapability("appium:udid", "R5CT20473PP");
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
         desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
@@ -29,23 +30,34 @@ public class AppiumTest {
         desiredCapabilities.setCapability("appPackage", "com.poincenot.bpn.mobile.qa");
         desiredCapabilities.setCapability("appActivity", "com.poincenot.doit.controller.activity.SplashActivity");
         desiredCapabilities.setCapability("autoGrantPermissions", "true");
+        desiredCapabilities.setCapability("disableWindowAnimation", "true");
 
         URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
     @Test
-    public void sampleTest() {
+    public void loginTest() {
 
 
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(LoginPage.userInput));
 
-        driver.findElement(LoginPage.userInput).sendKeys("test");
-        driver.findElement(LoginPage.passwordInput).sendKeys("test");
+
+        driver.findElement(LoginPage.userInput).sendKeys("rioshugo");
+        driver.findElement(LoginPage.passwordInput).sendKeys("QAbpn2022");
         driver.findElement(LoginPage.submitButton).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(LoginPage.accept_check));
+        driver.findElement(LoginPage.accept_check).click();
+        driver.findElement(LoginPage.accept_button).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginPage.txtWelcome));
+
+        Assert.assertTrue(driver.findElement(LoginPage.txtWelcome).getText().contains("HUGO"));
+
     }
 
     @AfterTest
--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -2,7 +2,16 @@ import org.openqa.selenium.By;
 
 public class LoginPage {
 
-    public static final By userInput = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[1]");
-    public static final By passwordInput = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[2]");
-    public static final By submitButton = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[6]");
+
+    public static final By txtWelcome = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.TextView[1]");
+
+    public static final By userInput = By.xpath("//*[@resource-id='username_test']");
+    public static final By passwordInput = By.xpath("//*[@resource-id='password_test']");
+    public static final By submitButton = By.xpath("//*[@resource-id='testID']");
+
+    public static final By accept_check = By.xpath("//*[@resource-id='terms_checkbox_test']");
+
+    public static final By accept_button = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]");
+
+
 }
